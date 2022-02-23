2555264e7758271848fcec435b64d3541608fe54
Mariana Saraiva <marianascm8@gmail.com>
2022-02-22 23:05:04 -0300

    build: install tailwind and styled-component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
+// import './Header.css';
+import HeaderStyle from './styles.js';
 
 function Header() {
   return (
-    <div className='header-container'>
+    <HeaderStyle>
       <Link to={'/'}>
         <p>About</p>
       </Link>
@@ -14,7 +15,7 @@ function Header() {
       <Link to={'/contact'}>
         <p>Contact</p>
       </Link>
-    </div>
+    </HeaderStyle>
   );
 }
 
---
+++ src/components/styles.js
@@ -0,0 +1,14 @@
+import tw from "tailwind-styled-components";
+
+const HeaderStyle = tw.header`
+  flex
+  items-center
+  justify-around
+  flex-row
+  w-full
+  bg-pink-900
+  text-white
+  py-6
+`;
+
+export default HeaderStyle;
