c862ce144dda4f2959fda10935fad4836d2697ea
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:36:05 -0300

    feat: Style-component

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,14 +1,40 @@
 import tw from "tailwind-styled-components";
 
-const HeaderStyle = tw.header`
+export const HeaderStyle = tw.header`
   flex
   items-center
-  justify-around
+  justify-evenly
   flex-row
   w-full
-  bg-pink-900
-  text-white
+  text-pink-100
+  bg-rose-700
   py-6
+  text-xl
+  fixed
+  top-0
 `;
 
-export default HeaderStyle;
+export const HeaderStyleP = tw.a`
+  flex
+  items-center
+  justify-between
+`;
+
+export const FooterStyled = tw.footer`
+  text-center
+  relative
+  bottom-0
+  w-full
+  bg-rose-50
+  text-l
+  py-4
+  opacity-70
+`;
+
+// flex
+// items-center
+// flex-wrap
+// justify-between
+// flex-shrink-0
+
+// opacity-70
