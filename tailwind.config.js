35d46634aaff27a226cbe4d5c7b948c2bf80edfd
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:39:01 -0300

    feat: update config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,39 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  // theme: {
+  //   extend: {
+  //     // backgroundImage: {
+  //     //   'folder-about': "url('../src/images/folder.JPG')",
+  //     // }
+  //   },
+  // },
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    backgroundImage: {
+      'folder': "url('/src/images/folder.JPG')",
+      'folder1': "url('/src/images/folder1.jpg')",
+      'folder2': "url('/src/images/folder2.jpg')",
+      'folder3': "url('/src/images/folder3.jpg')",
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
