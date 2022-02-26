0e6757e13771f148d45b95c9b6cfaffd233243d3
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:36:38 -0300

    feat: Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Projects() {
+  const array = [ 'HTML', 'CSS', 'JavaScript', 'GitHub', 'Jest', 'React', 'Redux', 'Docker', 'MySQL' ];
   return (
     <div>
       <Header />
-      <h1>Page Projects</h1>
+      <section>
+        <h2>Technology and Tools</h2>
+        <ul>
+          {array.map((tech) => (
+            <li>
+              {tech}
+              {/* <img src={icon} alt="icon HTML"/> */}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2>Projects</h2>
+        <p>
+          Colocar button para selecionar o projeto
+          Colocar um texto sobre os projetos e selecionar 6 projetos par vincular. 
+        </p>
+      </section>
+      <Footer />
     </div>
   );
 }
