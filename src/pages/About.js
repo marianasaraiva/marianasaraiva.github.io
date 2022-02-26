5ad79d4d352188d40bdf90d2480f21c4dd607c05
Mariana Saraiva <marianascm8@gmail.com>
2022-02-26 02:48:46 -0300

    feat: fix photo profile

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,9 +20,14 @@ function About() {
         </button>
       </NameStyle>
       <AboutStyle>
-        <img class="h-30 w-30 rounded-full m-10" src={ perfil } alt="Foto de Perfil"/>
+        <img 
+          width="250px" 
+          class="h-15 w-15 rounded-full ml-10"
+          src={ perfil }
+          alt="Foto de Perfil"
+        />
         <div class="shadow-lg bg-white p-8 m-10 rounded-lg">
-          <h2 class="text-3xl pb-10 ">About me</h2>
+          <h2 class="text-2xl pb-10 text-rose-700">About me</h2>
           <div class="shadow-lg ">
             <p>
               I'm shy and talk a lot, it seems contradictory but it's exactly like that. With these characteristics I worked for years in the hospital area, learning to have resilience, a sense of urgency and to work as a team. And it was when I needed to transform the paper records to the system that I found myself in love with Technology. The teenager who had taken the entrance exam for computational mathematics came along and took care of me. Today I am a student of Web Development at Trybe.
