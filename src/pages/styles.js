17a0b2524712a912cc1cca61a97e062e8d431de8
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:36:22 -0300

    feat: Style-pages

---
+++ src/pages/styles.js
@@ -0,0 +1,33 @@
+import tw from "tailwind-styled-components";
+// import folder from '../images/folder.JPG';
+
+export const AboutStyle = tw.section`
+  flex
+  items-center
+  justify-center
+  flex-row
+  text-center
+  text-l
+  bg-rose-700
+  w-full
+  h-screen
+  tracking-tight
+`;
+
+export const NameStyle = tw.section`
+  flex
+  items-center
+  justify-center
+  flex-col
+  text-xl
+  bg-folder2
+  backdrop-opacity-8
+  bg-center
+  bg-no-repeat
+  bg-cover
+  w-full
+  text-white
+  h-screen
+  tracking-tight
+  leading-10
+`;
