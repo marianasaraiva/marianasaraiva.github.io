cb3242b5bf8abd76420e8fef4b91d16324229508
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:35:27 -0300

    feat: Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Contact() {
@@ -6,6 +7,14 @@ function Contact() {
     <div>
       <Header />
       <h1>Page Contact</h1>
+      <section>
+        <h2>Find me here</h2>
+        <p>
+          I invite you to see my projects here:
+          Colocar LinkedIn, GitHub, Codewars
+        </p>
+      </section>
+      <Footer />
     </div>
   );
 }
