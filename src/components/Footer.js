6ad2cc28817fc6c35e49db8eea4d961310c1a7ef
Mariana Saraiva <marianascm8@gmail.com>
2022-02-25 23:34:23 -0300

    feat: Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterStyled } from './styles.js';
+
+function Footer() {
+  return (
+    <FooterStyled>
+        <p>Create for Mariana Saraiva | 2022</p>
+    </FooterStyled>
+  );
+}
+
+export default Footer;
