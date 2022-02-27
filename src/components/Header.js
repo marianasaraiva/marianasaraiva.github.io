6b3e87d632058fd8a467e6a2aeb0d24e800e1aa6
Mariana Saraiva <marianascm8@gmail.com>
2022-02-27 13:05:57 -0300

    feat: header position

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import { HeaderStyle, HeaderStyleP } from './styles.js';
 function Header() {
   return (
     <HeaderStyle>
-      <Link to={'/'}>Mariana Saraiva</Link>
+      <Link class="w-2/4 flex flex-wrap items-center justify-between hover:text-rose-500 duration-500" to={'/'}>Mariana Saraiva</Link>
       <HeaderStyleP>
-        <Link to={'/projects'}>Projects</Link>
-        <Link to={'/contact'}>Contact</Link>
+        <Link class="hover:text-rose-500 duration-500" to={'/projects'}>Projects</Link>
+        <Link class="hover:text-rose-500 duration-500" to={'/contact'}>Contact</Link>
       </HeaderStyleP>
     </HeaderStyle>
   );
