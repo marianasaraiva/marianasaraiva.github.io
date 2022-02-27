ee0124f6e84ecad4dff9a39f62f918d8bf59fffc
Mariana Saraiva <marianascm8@gmail.com>
2022-02-27 13:09:17 -0300

    feat: add data projects

---
+++ src/services/dataProjects.js
@@ -0,0 +1,52 @@
+import Pixel from '../images/pixel.png';
+import Trybewarts from '../images/trybewarts.png';
+import ShoppingCart from '../images/shoppingCart.png';
+import SolarSistem from '../images/solarSistem.png';
+import Trivia from '../images/trivia.png';
+import AppRecipe from '../images/appRecipe.png';
+import TrybeWallet from '../images/trybeWallet.png';
+
+export const dataProjects = [
+  {
+    name: 'Pixel Artes',
+    image: Pixel,
+    colaborate: [],
+    tecnologies: [ 'HTML', 'CSS' ],
+  },
+  {
+    name: 'Trybewarts',
+    image: Trybewarts,
+    colaborate: [ 'Juliana Miranda' ],
+    tecnologies: [ 'HTML', 'CSS' ],
+  },
+  {
+    name: 'Shopping Cart',
+    image: ShoppingCart,
+    colaborate: [],
+    tecnologies: [ 'HTML', 'CSS' ],
+  },
+  {
+    name: 'Solar Sistem',
+    image: SolarSistem,
+    colaborate: [],
+    tecnologies: [ 'React' ],
+  },
+  {
+    name: 'TrybeWallet',
+    image: TrybeWallet,
+    colaborate: [],
+    tecnologies: [ 'React', 'Redux' ],
+  },
+  {
+    name: 'Trivia',
+    image: Trivia,
+    colaborate: [ 'Alissandra Lima', 'Ruan Cunha', 'Samuel Gonçalves' ],
+    tecnologies: [ 'React', 'Redux' ],
+  },
+  {
+    name: 'App Recipe',
+    image: AppRecipe,
+    colaborate: [ 'Airton Lopes', 'Bruno Faria', 'Renata Domingues' ],
+    tecnologies: [ 'React', 'ContextAPI', 'Hooks' ],
+  },
+];
