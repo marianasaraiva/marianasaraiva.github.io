06108fd1f4cd7d2c0fe924eb141f2b061d29c1b9
Mariana Saraiva <marianascm8@gmail.com>
2022-03-02 20:57:06 -0300

    feat: rename route about to home

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import About from '../pages/About';
+import Home from '../pages/Home';
 import Contact from '../pages/Contact';
 import Projects from '../pages/Projects';
 
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={ About } />
+      <Route exact path="/" component={ Home } />
       <Route exact path="/contact" component={ Contact } />
       <Route exact path="/projects" component={ Projects } />
     </Switch>
