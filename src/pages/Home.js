96e16144baf41e0a3b7a81fe9e7a9c08c247344a
Mariana Saraiva <marianascm8@gmail.com>
2022-03-02 20:54:58 -0300

    feat: add page home to replace about and delete page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { AboutStyle, NameStyle} from './styles.js';
 import perfil from '../images/perfil.jpeg';
 
-function About() {
+function Home() {
+  const history = useHistory();
   return (
     <div>
       <Header />
@@ -14,6 +16,7 @@ function About() {
         <button
           class="border-solid border bg-rose-700 text-white mt-10 p-2 rounded-lg hover:bg-green-900 duration-500"
           type="button"
+          onClick={() => history.push('/about')}
         >
           Find out more
         </button>
@@ -25,8 +28,8 @@ function About() {
           src={ perfil }
           alt="Foto de Perfil"
         />
-        <div class="shadow-lg bg-white p-8 m-10 rounded-lg">
-          <h2 class="text-2xl pb-6 text-rose-700">About me</h2>
+        <div class="shadow-lg bg-black text-white p-8 m-10 rounded-lg">
+          <h2 class="text-2xl pb-6 text-rose-900">About me</h2>
           <div>
             <p>
               I'm shy and talk a lot, it seems contradictory but it's exactly like that. With these characteristics I worked for years in the hospital area, learning to have resilience, a sense of urgency and to work as a team. And it was when I needed to transform the paper records to the system that I found myself in love with Technology. The teenager who had taken the entrance exam for computational mathematics came along and took care of me. Today I am a student of Web Development at Trybe.
@@ -40,4 +43,4 @@ function About() {
   );
 }
 
-export default About;
+export default Home;
