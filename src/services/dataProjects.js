f322de3f3b8b355bbe1500f81ccff81b7850d996
Mariana Saraiva <marianascm8@gmail.com>
2022-03-06 08:53:33 -0300

    feat: add technologies file dataProjects

--- src/services/dataProjects.js
+++ src/services/dataProjects.js
@@ -10,50 +10,89 @@ export const dataProjects = [
   {
     name: 'Pixel Arts',
     image: Pixel,
-    colaborate: [],
+    collaborative: [],
     tecnologies: [ 'HTML', 'CSS', 'JavaScript' ],
     url: "https://github.com/tryber/sd-016-b-project-pixels-art/tree/mariana-saraiva-project-pixels-arts",
   },
   {
     name: 'Trybewarts',
     image: Trybewarts,
-    colaborate: [ 'Juliana Miranda' ],
+    collaborative: [ 'Juliana Miranda' ],
     tecnologies: [ 'HTML', 'CSS', 'JavaScript' ],
     url: "https://github.com/tryber/sd-016-b-project-trybewallet/tree/mariana-saraiva-project-trybewallet",
   },
   {
     name: 'Shopping Cart',
     image: ShoppingCart,
-    colaborate: [],
+    collaborative: [],
     tecnologies: [ 'HTML', 'CSS', 'JavaScript' ],
     url: "https://github.com/tryber/sd-016-b-project-shopping-cart/tree/mariana-saraiva-shopping-cart",
   },
   {
     name: 'Solar System',
     image: SolarSistem,
-    colaborate: [],
+    collaborative: [],
     tecnologies: [ 'React' ],
     url: "https://github.com/tryber/sd-016-b-project-solar-system/tree/mariana-saraiva-project-solar-system",
   },
   {
     name: 'TrybeWallet',
     image: TrybeWallet,
-    colaborate: [],
+    collaborative: [],
     tecnologies: [ 'React', 'Redux' ],
     url: "https://github.com/tryber/sd-016-b-project-trybewallet/tree/mariana-saraiva-project-trybewallet",
   },
   {
     name: 'Trivia',
     image: Trivia,
-    colaborate: [ 'Alissandra Lima', 'Ruan Cunha', 'Samuel Gonçalves' ],
+    collaborative: [ 'Alissandra Lima', 'Ruan Cunha', 'Samuel Gonçalves' ],
     tecnologies: [ 'React', 'Redux' ],
     url: "https://github.com/tryber/sd-016-b-project-trivia-react-redux/tree/main-group-15",
   },
   {
     name: 'App Recipe',
     image: AppRecipe,
-    colaborate: [ 'Airton Lopes', 'Bruno Faria', 'Renata Domingues' ],
+    collaborative: [ 'Airton Lopes', 'Bruno Faria', 'Renata Domingues' ],
     tecnologies: [ 'React', 'ContextAPI', 'Hooks' ],
     url: "https://github.com/tryber/sd-016-b-project-recipes-app/tree/main-group-3",
+  },
+];
+
+export const dataTechnologies = [
+  {
+    name: 'HTML',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+  },
+  {
+    name: 'CSS',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+  },
+  {
+    name: 'JavaScript',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: 'GitHub',
+    image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+  },
+  {
+    name: 'Jest',
+    image: "https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg",
+  },
+  {
+    name: 'React',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  },
+  {
+    name: 'Redux',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+  },
+  {
+    name: 'Docker',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+  },
+  {
+    name: 'MySQL',
+    image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
   },
 ];
